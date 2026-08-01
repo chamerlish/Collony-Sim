b0c10743a3a50ab35c1c0678d3605be5a3362854
chamerli <fares.setbel@proton.me>
2026-08-01 16:37:49 +0100

    fixed the double selection and added a sprite what?

--- scripts/minion.gd
+++ scripts/minion.gd
@@ -7,25 +7,39 @@ enum characterState{
 	SELECTED
 }
 
-const MAX_SPEED = 50
-const SPEED_DIVIDER = 2
+const MAX_SPEED: float = 50
+const SPEED_DIVIDER: float = 2
 
 var current_state: characterState
 var target_position: Vector2
-var current_speed: int
-var selected: bool
+var current_speed: float
+var selected: bool = false:
+	set(value):
+		if value:
+			current_speed = MAX_SPEED / SPEED_DIVIDER
+			$SelectionOutline.show()
+		else: 
+			current_speed = MAX_SPEED
+			$SelectionOutline.hide()
+		selected = value
 
 func check_select() -> void:
 	if Input.is_action_just_pressed("click"):
-		if get_global_mouse_position().distance_to(global_position) < 15:
-			selected = true
-			current_speed = MAX_SPEED / SPEED_DIVIDER
+		var mouse_position = get_global_mouse_position()
+		if mouse_position.distance_to(global_position) < 25:
+			selected = !selected
+			return
+		if selected:
+			target_position = mouse_position
+			current_state = characterState.WALKING
+			selected = false
+		
+
 func state_machine() -> void:
 	match current_state:
 		characterState.IDLE:
 			check_select()
 		characterState.WALKING:
-			current_speed = MAX_SPEED
 			check_select()
 			var direction = (target_position - global_position).normalized()
 			velocity = direction * current_speed
@@ -34,31 +48,26 @@ func state_machine() -> void:
 				current_state = characterState.IDLE
 		characterState.WORKING:
 			pass
-		characterState.SELECTED:
-			if Input.is_action_just_pressed("click"):
-				target_position = get_global_mouse_position()
-				current_state = characterState.WALKING
-			elif target_position.distance_to(global_position) < 1:
-				velocity = Vector2.ZERO
-				current_state = characterState.IDLE
+		#characterState.SELECTED:
+		#	if Input.is_action_just_pressed("click"):
+		#		target_position = get_global_mouse_position()
+		#		current_state = characterState.WALKING
+		#	elif target_position.distance_to(global_position) < 1:
+		#		velocity = Vector2.ZERO
+		#		current_state = characterState.IDLE
 
 func state_machine2() -> void:
 	match current_state:
 		characterState.IDLE:
 			check_select()
-			if selected == true and Input.is_action_just_pressed("click"):
-				print("set to walking")
-				target_position = get_global_mouse_position()
-				current_state = characterState.WALKING
 		characterState.WALKING:
-			current_speed = MAX_SPEED
 			check_select()
 			var direction = (target_position - global_position).normalized()
 			velocity = direction * current_speed
 			if target_position.distance_to(global_position) < 1:
 				velocity = Vector2.ZERO
 				current_state = characterState.IDLE
-				selected = false
+				
 		characterState.WORKING:
 			pass
 
@@ -71,5 +80,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	state_machine2()
-	print(current_state, selected)
 	move_and_slide()
